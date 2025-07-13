7c1e41b11871460425f90f7a992f04cf15ad550b
Abhishek Tiwari <abhishekt@idmgroup.co.za>
2025-07-13 14:06:28 +0530

    Update

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+// Add configuration
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
