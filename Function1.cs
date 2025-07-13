bf9465e1e75db2e7b401eaec8fae79a6aaf1988e
Abhishek Tiwari <abhishekt@idmgroup.co.za>
2025-07-13 13:47:11 +0530

    Repo creation

---
+++ Function1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp2
+{
+    internal class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("Ping")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+        {
+            _logger.LogInformation("Ping function executed.");
+            
+            return new OkObjectResult("pong");
+        }
+    }
+}
